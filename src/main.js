bbacc8f1a02359a22685a5812e2b8d054f8a2d73
alanguilhermeM <alanguilherme13@outlook.com>
2023-03-22 15:26:26 -0300

    Finalizando requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
 import './style.css';
+import { createProductElement } from './helpers/shopFunctions';
+
+const computadores = await fetchProductsList('computador');
+const products = document.querySelector('.products');
+
+const lista = async () => {
+  computadores.forEach((computador) => products
+    .appendChild(createProductElement(computador)));
+};
+
+lista();
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
